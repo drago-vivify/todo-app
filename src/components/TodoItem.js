9c5e8f653347a9fc4c221b1f48acd4aa1774e3e9
Drago Stevanovic <drago.s@vivifyideas.com>
2019-05-19 01:43:03 +0200

    Add TodoList and TodoItem components

---
+++ src/components/TodoItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function CheckedTodoItem({ todo }) {
+  return (
+    <div className="list-group-item list-group-item-success">
+      <span className="fa fa-check" />
+      <span style={{ textDecoration: 'line-through' }}>{todo.title}</span>
+    </div>
+  );
+}
+
+function UncheckedTodoItem({ todo }) {
+  return (
+    <div className="list-group-item">
+      <span>{todo.title}</span>
+    </div>
+  );
+}
+
+export default function TodoItem({ todo, onTodoClick }) {
+  return (
+    <div onClick={onTodoClick}>
+      {todo.isChecked ? <CheckedTodoItem todo={todo} /> : <UncheckedTodoItem todo={todo} />}
+    </div>
+  );
+}
